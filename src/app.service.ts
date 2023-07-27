1b736574cba72415075c238ea3b82eac51a60942
Abhishek Poudel <abhishekpoudel84@gmail.com>
2023-07-27 11:15:28 +0545

    cron job

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,57 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { CreateUser } from './dto/create_user.dto';
+import { EventEmitter2, OnEvent } from '@nestjs/event-emitter';
+import { UserCreatedEvent } from './events/user_created.event';
+import { Cron, CronExpression, SchedulerRegistry } from '@nestjs/schedule';
 
 @Injectable()
 export class AppService {
+  constructor(
+    private readonly eventEmitter: EventEmitter2,
+    private readonly schedulerRegistry: SchedulerRegistry,
+  ) {}
+  private readonly logger = new Logger(AppService.name);
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async createUser(body: CreateUser) {
+    this.logger.log('Creating User...', body);
+    const userId = '123';
+    this.eventEmitter.emit(
+      'user.created',
+      new UserCreatedEvent(userId, body.email),
+    );
+    const establishWsTimeout = setTimeout(
+      () => this.establishWsConnection(userId),
+      5000,
+    );
+
+    this.schedulerRegistry.addTimeout(
+      `${userId}_establish_ws`,
+      establishWsTimeout,
+    );
+  }
+
+  private establishWsConnection(userId: string) {
+    this.logger.log('Establishing ws connection with user...', userId);
+  }
+
+  @OnEvent('user.created')
+  welcomeNewUser(payload: UserCreatedEvent) {
+    this.logger.log('Welcoming New User.', payload.email);
+  }
+
+  @OnEvent('user.created', { async: true })
+  async sendWelcomeGift(payload: UserCreatedEvent) {
+    this.logger.log('Sending welcoming gift...', payload.email);
+    await new Promise<void>((resolve) => setTimeout(() => resolve(), 3000));
+    this.logger.log('Welcome gift sent.', payload.email);
+  }
+
+  @Cron(CronExpression.EVERY_10_MINUTES, { name: 'delete_expired-users' })
+  deleteExpiredUsers() {
+    this.logger.log('Deleting expired users...');
   }
 }
